aedd22fe5fe0031d44f3cec0886ccc8827676db4
ParvaCygbit <parva.shah@cygbit.com>
2026-03-13 18:46:27 +0530

    added new git commands

--- src/GitIntro.js
+++ src/GitIntro.js
@@ -2,7 +2,7 @@
 // --> source ~/.zshrc
 
 // git ia a version control system that helps developers to track changes in their code and collaborate with other developers
-// 1) trch the history of changes in the code
+// 1) track the history of changes in the code
 // 2) collaborate with other developers
 // 3) manage different versions of the code
 
@@ -37,21 +37,51 @@
 
 // now there are 2 words for commod in git is remote and local
 // local means the changes that are made in the local repository on your system and remote means the changes that are made in the remote repository on github.
+// there 4 file statuses of files in git 
+// 1) untracked --> file is not in git history to track , 2) modified --> file is changed , 3) staged --> file is ready to commit , 4) unmodified --> file is not changed
+// git workflow is 1) github repo --> 2) clone the repo in local system --> 3) make changes in the code --> 4) stage the changes --> 5) commit the changes --> 6) push the changes to remote repo on github.
 
+
+// this is from github to local system
 // > git clone <repository_url> --> it is clone a remote repository in local system.
+
+
+// this is from local system to github
+// > git init --> it is a command to initialize a new git repo in the current directory.
+// > "git remote add origin <remote_repository_url>" --> it is a command to connect local repo with remote repo. we have to add the remote repo to our local repo using the command. origin is a default name for the remote repository, but you can choose any name you want.
+// > git remote -v --> it is a command to check the remote repository that is connected to the local repository.
+
+
 // > git status --> it is display the state of the code
-// > git init --> it is a command to initialize a new git repository in the current directory.
-
 // > git add . --> it is a command to stage all the changes in the current directory and its subdirectories.
+// > git add <file_name> --> it is a command to stage the changes in the specified file.
+// > git restore <file_name> --> it is a command to unstage the changes in the specified file.
+// > git restore --staged <file_name> --> it is a command to unstage the changes in the specified file after adding file and also restore the file to its previous state.
 // > git commit -m "message" --> it is a command to commit the staged changes with a message that describes the changes.
-// > git push origin main --> it is a command to push the committed changes to the remote repository on github. "origin" is the name of the remote repository and "main" is the name of the branch.
+// > git push origin main --> it is a command to upload committed local changes to the remote repo on github. 
+// "origin" is the name of the remote repo and "main" is the name of the branch.
+// > git push -u origin main --> it is a command to push the changes to the remote repo and also set the upstream branch. it is used when we want to push the changes for the first time to the remote repo. after running this command, we can simply run "git push" to push the changes in future without specifying the remote and branch name.
 
 
-// to connect local repository with remote repository we have to add the remote repository to our local repository using the command "git remote add origin <remote_repository_url>".
-// after connecting the local repository with remote repository we can push the changes from local repository to remote repository using the command "git push origin main" where "origin" is the name of the remote repository and "main" is the name of the branch.
+// now we work on branches and merge the branches
+// > git branch --> it is a command to list all the branches in the local repository and working branch.
+// > git branch -M main --> it is a command to rename the current branch to main. it is used when we want to change the name of the default branch from master to main.
+// > git checkout <branch_name> --> it is a command to switch to the specified branch. 
+// it is used when we want to work on a different branch or when we want to merge the changes from one branch to another.
+
+// > git checkout -b <branch_name> --> it is a command to create a new branch and switch to that branch. 
+// it is used when we want to work on a new feature or bug fix without affecting the main branch.
+
+// > git branch -d <branch_name> --> it is a command to delete the specified branch. it is not delete current branch.
+// it is used when we want to delete a branch that is no longer needed or when we want to clean up the branches in the repository.
+
+// > git branch -D <branch_name> --> it is a command to force delete the specified branch.
+// it is used when we want to delete a branch that has unmerged changes or when we want to delete a branch that is not fully merged with the main branch.
+
+// > git merge <branch_name> --> it is a command to merge the specified branch into the current branch. 
+// it is used when we want to combine the changes from one branch to another. for example, when we want to merge a feature branch into the main branch after completing the work on that feature.
 
 
 
-
-
+// extra commonds
 // ls -a --> shows all the files and folders in the current directory including hidden files and folders.
